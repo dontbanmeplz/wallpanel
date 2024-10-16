749937155316ee37aebf9d0921fce467db144d94
Chis <59851616+dontbanmeplz@users.noreply.github.com>
2024-10-16 18:13:13 -0400

    Update bef.js

--- bef.js
+++ bef.js
@@ -8,7 +8,171 @@ function mtms(millis) {
 	var seconds = ((millis % 60000) / 1000).toFixed(0);
 	return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
 }
+
 const redirect_uri = "https://dash.sawicz.com"; // Your redirect uri
+const client_id = "ce4ca56cf45348c3af1a7368942eda8c";
+function generateRandomString(length) {
+	let text = "";
+	const possible =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+	for (let i = 0; i < length; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	return text;
+}
+
+async function generateCodeChallenge(codeVerifier) {
+	const digest = await crypto.subtle.digest(
+		"SHA-256",
+		new TextEncoder().encode(codeVerifier),
+	);
+
+	return btoa(String.fromCharCode(...new Uint8Array(digest)))
+		.replace(/=/g, "")
+		.replace(/\+/g, "-")
+		.replace(/\//g, "_");
+}
+
+function generateUrlWithSearchParams(url, params) {
+	const urlObject = new URL(url);
+	urlObject.search = new URLSearchParams(params).toString();
+
+	return urlObject.toString();
+}
+
+function redirectToSpotifyAuthorizeEndpoint() {
+	const codeVerifier = generateRandomString(64);
+
+	generateCodeChallenge(codeVerifier).then((code_challenge) => {
+		window.localStorage.setItem("code_verifier", codeVerifier);
+
+		// Redirect to example:
+		// GET https://accounts.spotify.com/authorize?response_type=code&client_id=77e602fc63fa4b96acff255ed33428d3&redirect_uri=http%3A%2F%2Flocalhost&scope=user-follow-modify&state=e21392da45dbf4&code_challenge=KADwyz1X~HIdcAG20lnXitK6k51xBP4pEMEZHmCneHD1JhrcHjE1P3yU_NjhBz4TdhV6acGo16PCd10xLwMJJ4uCutQZHw&code_challenge_method=S256
+
+		window.location = generateUrlWithSearchParams(
+			"https://accounts.spotify.com/authorize",
+			{
+				response_type: "code",
+				client_id,
+				scope:
+					"ugc-image-upload user-read-playback-state user-modify-playback-state user-read-currently-playing app-remote-control streaming playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public user-follow-modify user-follow-read user-read-playback-position user-top-read user-read-recently-played user-library-modify user-library-read user-read-email user-read-private",
+				code_challenge_method: "S256",
+				code_challenge,
+				redirect_uri,
+			},
+		);
+
+		// If the user accepts spotify will come back to your application with the code in the response query string
+		// Example: http://127.0.0.1:8080/?code=NApCCg..BkWtQ&state=profile%2Factivity
+	});
+}
+
+function exchangeToken(code) {
+	const code_verifier = localStorage.getItem("code_verifier");
+
+	fetch("https://accounts.spotify.com/api/token", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+		},
+		body: new URLSearchParams({
+			client_id,
+			grant_type: "authorization_code",
+			code,
+			redirect_uri,
+			code_verifier,
+		}),
+	})
+		.then(addThrowErrorToFetch)
+		.then((data) => {
+			processTokenResponse(data);
+
+			// clear search query params in the url
+			window.history.replaceState({}, document.title, "/");
+		})
+		.catch(handleError);
+}
+
+function refreshToken() {
+	refresh_token = localStorage.getItem("refresh_token");
+	fetch("https://accounts.spotify.com/api/token", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+		},
+		body: new URLSearchParams({
+			grant_type: "refresh_token",
+			refresh_token: refresh_token,
+			client_id: client_id,
+		}),
+	})
+		.then(addThrowErrorToFetch)
+		.then(processTokenResponse)
+		.catch(handleError1);
+}
+
+function handleError(error) {
+	console.error(error);
+}
+function handleError1(error) {
+	console.error(error);
+	console.log(error.error.error_description);
+	if (error.error.error_description === "Invalid refresh token") {
+		localStorage.clear();
+		window.location.href = "http://localhost:8000/";
+	}
+}
+async function addThrowErrorToFetch(response) {
+	if (response.ok) {
+		return response.json();
+	} else {
+		throw { response, error: await response.json() };
+	}
+}
+
+function logout() {
+	localStorage.clear();
+	window.location.reload();
+}
+
+function processTokenResponse(data) {
+	access_token = data.access_token;
+	refresh_token = data.refresh_token;
+
+	const t = new Date();
+	expires_at = t.setSeconds(t.getSeconds() + 3600);
+
+	localStorage.setItem("access_token", access_token);
+	localStorage.setItem("refresh_token", refresh_token);
+	localStorage.setItem("expires_at", expires_at);
+
+	// load data of logged in user
+	getUserData();
+}
+
+function getUserData() {
+	fetch("https://api.spotify.com/v1/me", {
+		headers: {
+			Authorization: "Bearer " + access_token,
+		},
+	})
+		.then(async (response) => {
+			if (response.ok) {
+				return response.json();
+			} else {
+				throw await response.json();
+			}
+		})
+		.then((data) => {})
+		.catch((error) => {
+			//refreshToken();
+		});
+}
+
+// Your client id from your app in the spotify dashboard:
+// https://developer.spotify.com/dashboard/applications
+
 // Restore tokens from localStorage
 var access_token = localStorage.getItem("access_token") || null;
 var refresh_token = localStorage.getItem("refresh_token") || null;
